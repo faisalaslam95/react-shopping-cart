f60ae4a2229b9986db74e1253ec45a6068a48448
faisal <faisalaslam1995@gmiail.com>
2021-05-25 19:28:57 +0500

    setup

---
+++ app/src/App.js
@@ -0,0 +1,19 @@
+
+
+function App() {
+  return (
+    <div className="App grid-container">
+      <header className="app-header">
+        <a href="/">React Shopping Cart</a>
+      </header>
+      <main>
+        Product List
+      </main>
+      <footer>
+        All right is reserved
+      </footer>
+    </div>
+  );
+}
+
+export default App;
